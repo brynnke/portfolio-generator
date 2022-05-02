1e799c27e71ef33c13c68b6542354fdd90e39fda
brynnke <100329799+brynnke@users.noreply.github.com>
2022-05-01 21:25:30 -0500

    added most of needed lesson 3 items, including gitignore and node mod

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const fs = require("fs");
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
-const [name, github] = profileDataArgs;
-console.log(name, github);
-const pageHtml = generatePage(name,github);
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// const fs = require("fs");
+const inquirer = require('inquirer');
+console.log(inquirer);
+//const generatePage = require('./src/page-template');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+ 
+//const [name, github] = profileDataArgs;
+//console.log(name, github);
+//const pageHtml = generatePage(name,github);
+//fs.writeFile('index.html', generatePage(name, github), err => {
+ // if (err) throw err;
+
+//  console.log('Portfolio complete! Check out index.html to see the output!');
+// });
